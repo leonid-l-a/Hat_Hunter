e294f879762b5976765db664cc7e4ba6bc7cb13e
n1k1ttka <nikita.kolgushkin@mail.ru>
2025-09-24 20:37:10 +0300

    Перенес нарушение зависимости у FavoritesInteractor.kt, перенес обработку исключений в data-слой

--- app/src/main/java/ru/practicum/android/diploma/favorites/domain/repository/FavoritesRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/repository/FavoritesRepository.kt
@@ -2,15 +2,18 @@ package ru.practicum.android.diploma.favorites.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.favorites.domain.model.VacancyFavorites
+import ru.practicum.android.diploma.favorites.domain.state.Result
 import ru.practicum.android.diploma.vacancy.domain.model.VacancyDetail
 
 interface FavoritesRepository {
 
-    fun favoritesVacancy(): Flow<List<VacancyFavorites?>>
+    fun favoritesVacancy(): Flow<Result<List<VacancyFavorites?>>>
 
-    suspend fun addFavorite(vacancy: VacancyDetail): Long
+    suspend fun addFavorite(vacancy: VacancyDetail): Result<Long>
 
-    suspend fun findFavorite(id: String): VacancyFavorites?
+    suspend fun findFavorite(id: String): Result<VacancyFavorites?>
 
-    suspend fun deleteFromFavorites(vacancy: VacancyDetail)
+    suspend fun deleteFromFavorites(vacancy: VacancyDetail): Result<Unit>
+
+    suspend fun findFavoriteForDetail(id: String): Result<VacancyDetail?>
 }
--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodel/FavoritesViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodel/FavoritesViewModel.kt
@@ -2,12 +2,11 @@ package ru.practicum.android.diploma.favorites.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.sqlite.SQLiteException
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.favorites.domain.interactor.FavoritesInteractor
+import ru.practicum.android.diploma.favorites.domain.state.Result
 import ru.practicum.android.diploma.favorites.ui.state.FavoriteState
 import ru.practicum.android.diploma.util.DebounceUtil
 
@@ -28,22 +27,24 @@ class FavoritesViewModel(
     init {
         viewModelScope.launch {
             favoritesInteractor.getFavoritesVacancy()
-                .catch { e ->
-                    if (e is SQLiteException) {
-                        _stateFavoritesVacancy.value = FavoriteState.Error(e.message)
-                    } else {
-                        throw e
-                    }
-                }
-                .collect { favorites ->
-                    val nonNullFavorites = favorites.filterNotNull()
-                    _stateFavoritesVacancy.value = when {
-                        nonNullFavorites.isEmpty() -> FavoriteState.Empty
-                        else -> FavoriteState.Content(nonNullFavorites)
+                .collect { result ->
+                    when (result) {
+                        is Result.Success -> {
+                            val nonNullFavorites = result.data.filterNotNull()
+                            _stateFavoritesVacancy.value = when {
+                                nonNullFavorites.isEmpty() -> FavoriteState.Empty
+                                else -> FavoriteState.Content(nonNullFavorites)
+                            }
+                        }
+
+                        is Result.Error -> {
+                            _stateFavoritesVacancy.value = FavoriteState.Error(result.message)
+                        }
                     }
                 }
         }
     }
+
 
     fun clickDebounce(): Boolean {
         val current = isClickAllowed
